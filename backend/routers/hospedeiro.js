60465cc983918d869973796383664f6d831b9129
Alexandre de Jesus <agladejesus@gmail.com>
2022-02-18 16:41:12 -0400

    adjusting routes and controls in the host table

--- backend/routers/hospedeiro.js
+++ backend/routers/hospedeiro.js
@@ -4,7 +4,7 @@ var router = require("express").Router();
 module.exports = app => {
     var _hospedeiro = require("../controllers/hospedeiro");
 
-  // Create a new Tutorial
+  // Create a new hospedeiro
   router.post("/", _hospedeiro.create);
 
   // Retrieve all _hospedeiro
@@ -13,13 +13,13 @@ module.exports = app => {
   // Retrieve all published _hospedeiro
   router.get("/search", _hospedeiro.findAllParam);
 
-  // Retrieve a single Tutorial with id
+  // Retrieve a single hospedeiro with id
   router.get("/:id", _hospedeiro.findOne);
 
-  // Update a Tutorial with id
+  // Update a hospedeiro with id
   router.put("/:id", _hospedeiro.update);
 
-  // Delete a Tutorial with id
+  // Delete a hospedeiro with id
   router.delete("/:id", _hospedeiro.delete);
 
   // Delete all _hospedeiro

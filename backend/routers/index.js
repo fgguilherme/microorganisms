cf9de2d6eac2f6bff3870c27c651ad48833015de
Alexandre de Jesus <agladejesus@gmail.com>
2022-02-21 13:44:37 -0400

    adding routes and controls in repique_has_repique table

--- backend/routers/index.js
+++ backend/routers/index.js
@@ -44,4 +44,5 @@ module.exports = app => {
   require("./microorganismo_has_metodo_preservacao")(app);
   require("./repique_has_imagem")(app);
   require("./repique_has_referencia")(app);
+  require("./repique_has_repique")(app);
 }
